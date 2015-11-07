24d3ca294012654ecd88638434362145abea7398
charlie <charlie@macbook-pro.home>
2015-11-07 18:03:55 -0500

    First commit

---
+++ src/pythonscratch.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="charlie"
+__date__ ="$Nov 7, 2015 5:54:30 PM$"
+
+if __name__ == "__main__":
+    print "Hello Charlie World"
